489255ff5187b44c1e8bc9892754e969c3598163
Rohit Vishwakrma <rv20823@gmail.com>
2024-03-31 19:22:45 +0530

    gui fix terminal

--- components/terminal/py_terminal.py
+++ components/terminal/py_terminal.py
@@ -2,14 +2,17 @@ import os
 import subprocess
 from kivy.clock import Clock
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 
 Builder.load_file("./components/terminal/py_terminal.kv")
 
 
 class Terminal(BoxLayout):
     console_text = StringProperty(f"root@{os.getcwd()}$~ ")
+    initial_textinput_len_x = NumericProperty()
+
     # n = "root@user$~ "
     n = f"root@{os.getcwd()}$~ "
 
@@ -25,18 +28,37 @@ class Terminal(BoxLayout):
         last_line = all_line[-1]
 
         code = last_line.split(" ")
-
-        code.remove(f"root@{os.getcwd()}$~")
+        try:
+            code.remove(f"root@{os.getcwd()}$~")
+        except Exception as e:
+            return str(e)
         # '''
-        # users commands 1st index
+        # users commands 1st index to change directory
         user_commands = code[0]
 
         if user_commands == "cd":
-            # Change directory using os.chdir
-            directory = code[1]
-            os.chdir(directory)
-            self.n = f"root@{os.getcwd()}$~ "
-            return ""
+            try:
+                # Change directory using os.chdir
+                print(len(code))
+                if len(code) > 1:
+                    print(True)
+                    directory = code[1]
+                    os.chdir(directory)
+                    self.n = f"root@{os.getcwd()}$~ "
+                    return ""
+                else:
+                    try:
+                        target_dir = code[0]
+                        os.chdir(target_dir)
+                        print("done")
+                        return "done"
+                    except Exception as e:
+                        print(e)
+                        return str(e)
+                
+            except Exception as e:
+                print(e)
+                return str(e)
         # '''
         else:
             try:
@@ -50,5 +72,15 @@ class Terminal(BoxLayout):
 
     def focus_terminal(self, dt):
         self.ids.console.focus = True
+
+
+    def update_textinput_width(self, instance, textinput_width, text_width):
+        self.initial_textinput_len_x = textinput_width
+
+        if self.initial_textinput_len_x < text_width:
+            instance.width = (Window.width + text_width) 
+            self.initial_textinput_len_x = text_width
+        else:
+            instance.width = self.initial_textinput_len_x
 
 # runTouchApp(Terminal())
