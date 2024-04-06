b1d4d701349adf8480f3cfaceeec9038b9e2a908
Rohit Vishwakrma <rv20823@gmail.com>
2024-04-06 21:21:38 +0530

    added filepath implementation

--- actions/explorer_managment.py
+++ actions/explorer_managment.py
@@ -1,20 +1,26 @@
 import os, json, time
+from kivy.utils import platform
 
 def create_file_tree(folder_path):
 
     file_tree = {"name": os.path.basename(folder_path)}
     if os.path.isdir(folder_path):
         file_tree["type"] = "folder"
+        file_tree["source_path"] = f"{(folder_path)}"
         file_tree["children"] = [
             create_file_tree(os.path.join(folder_path, f)) for f in os.listdir(folder_path)
         ]
     else:
         file_tree["type"] = "file"
         file_tree["children"] = []
+        file_tree["source_path"] = f"{(folder_path)}"
     return file_tree
 
 # folder_path = "./dummy_files"
-folder_path = "C:/Users/user/CatxCode-ide"
+if platform == "win":
+    folder_path = "C:\\Users\\user\\CatxCode-ide"
+else:
+    folder_path = "/"
 file_tree = create_file_tree(folder_path)
 with open("tree.json", "w") as f:
     json.dump(file_tree, f)
--- actions/filepath_spliter.py
+++ actions/filepath_spliter.py
@@ -1,7 +1,16 @@
 from kivy.utils import platform
-
+file_name_and_path_dict = {}
 def file_name_(file_path):
     if platform == "win":
-        return file_path.split("\\")
+
+        file_name_extracted = file_path.split("\\")[-1]
+
+        file_name_and_path_dict["file_name"] = file_name_extracted
+        file_name_and_path_dict["file_path"] = file_path
+        return file_name_and_path_dict
     else:
-        return file_path.split("/")
+        file_name_extracted = file_path.split("/")[-1]
+
+        file_name_and_path_dict["file_name"] = file_name_extracted
+        file_name_and_path_dict["file_path"] = file_path
+        return file_name_and_path_dict
